7fa089228ac9ca17b5db44a8b2f7956a658964a8
mavegaq <manuel.93.vega@gmail.com>
2023-03-15 18:12:20 -0600

    Few new details, embellishing

--- src/App.jsx
+++ src/App.jsx
@@ -57,50 +57,82 @@ function App() {
     setDarkMode((prevState) => !prevState);
   };
 
-    return (
-      <div className={`App ${darkMode ? "dark" : "light"}`}>
-        <div className="content-container">
-          <input
-            type="text"
-            value={city}
-            onChange={(e) => setCity(e.target.value)}
-          />
-          <button onClick={searchByCity} className={`search-button ${darkMode ? "dark" : "light"}`}> Buscar </button>
-  
-          <div className={`card ${darkMode ? "dark" : "light"}`}>
-            <h1>{data.name}</h1>
-            {data.sys && <p>País: {data.sys.country}</p>}
-            {data.weather && (
-              <>
-                <p>{data.weather[0].description}</p>
-                <img
-                  src={`http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`}
-                  alt={data.weather[0].description}
-                />
-              </>
-            )}
-            {data.main && (
-              <p>
-                Temperatura:{" "}
-                {displayFahrenheit
-                  ? kelvinToFahrenheit(data.main.temp).toFixed(1)
-                  : kelvinToCelsius(data.main.temp).toFixed(1)}
-                °{displayFahrenheit ? "F" : "C"}
-              </p>
-            )}
-          </div>
-  
-          <button onClick={toggleTemperatureUnit} className={`toggle-temp-button ${darkMode ? "dark" : "light"}`}>
-            Cambiar a {displayFahrenheit ? "Celsius" : "Fahrenheit"}
-          </button>
-          
-          <button onClick={toggleDarkMode} className={`toggle-mode-button ${darkMode ? "dark" : "light"}`}>
-            Cambiar a {darkMode ? "Modo claro" : "Modo oscuro"}
-          </button>
-        </div>
-      </div>
-    );
-  }
-  
+  const getWeatherImage = (description, defaultIcon) => {
+    switch (description) {
+      case "clear sky":
+        return "1.svg";
+      case "few clouds":
+        return "2.svg";
+      case "scattered clouds":
+        return "3.svg";
+      case "broken clouds":
+        return "4.svg";
+      case "shower rain":
+        return "5.svg";
+      case "rain":
+        return "6.svg";
+      case "thunderstorm":
+        return "7.svg";
+      case "snow":
+        return "8.svg";
+      case "mist":
+        return "9.svg";
+      default:
+        return `http://openweathermap.org/img/wn/${defaultIcon}@2x.png`;
+    }
+  };
 
+  return (
+    <div className={`App ${darkMode ? "dark" : "light"}`}>
+      <div className="switch-wrapper">
+        <label className="switch">
+          <input
+            type="checkbox"
+            onChange={toggleDarkMode}
+            checked={darkMode}
+          />
+          <span className="slider round"></span>
+        </label>
+      </div>
+      <div className={`card ${darkMode ? "dark" : "light"}`}>
+        <input
+          type="text"
+          value={city}
+          onChange={(e) => setCity(e.target.value)}
+        />
+        <button onClick={searchByCity} className={`search-button ${darkMode ? "dark" : "light"}`}> Buscar </button>
+  
+        <h1>{data.name}</h1>
+        {data.sys && <p>País: {data.sys.country}</p>}
+        {data.weather && (
+           <>
+           <p>{data.weather[0].description}</p>
+           <img
+             src={getWeatherImage(
+               data.weather[0].description,
+               data.weather[0].icon
+             )}
+             alt={data.weather[0].description}
+           />
+         </>
+        )}
+        {data.main && (
+          <p>
+            Temperatura:{" "}
+            {displayFahrenheit
+              ? kelvinToFahrenheit(data.main.temp).toFixed(1)
+              : kelvinToCelsius(data.main.temp).toFixed(1)}
+            °{displayFahrenheit ? "F" : "C"}
+          </p>
+        )}
+  
+        <button onClick={toggleTemperatureUnit} className={`toggle-temp-button ${darkMode ? "dark" : "light"}`}>
+          Cambiar a {displayFahrenheit ? "Celsius" : "Fahrenheit"}
+        </button>
+      </div>
+    </div>
+  );
+  
+}
 export default App;
+
